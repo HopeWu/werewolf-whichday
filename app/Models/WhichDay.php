2fb8eafa144efd55528ee1dbe35f6a853c09ccab
HopeWu <767088240@qq.com>
2022-12-29 01:44:24 +0000

    fix bug about the latest

--- app/Models/WhichDay.php
+++ app/Models/WhichDay.php
@@ -11,12 +11,23 @@ class WhichDay extends Model
     public $guarded = [];
     protected $table = "which_day";
 
+    /*
+     * select which_day.wechat_name, which_day.which_day, which_day.time
+     * from which_day, (SELECT max(id) as id, wechat_name, max(created_at) FROM which_day GROUP BY wechat_name ) latest
+     * where which_day.id =  latest.id and activity_code = 0000;
+     */
+
     public static function votesOf($activity_code){
-        return self::select('wechat_name', DB::raw('MAX(which_day) as which_day'), DB::raw('MAX(time) as time'))
-            ->where('activity_code', '=', $activity_code)
-            ->groupBy('wechat_name')
-            ->get();
+        $sub = self::select('wechat_name', DB::raw("max(created_at) as latest_date"))
+            ->groupBy('wechat_name');
+        $data = self::join(DB::raw("({$sub->toSql()}) latest_table"), function ($join){
+            $join->on('latest_table.wechat_name', '=', 'which_day.wechat_name')
+            ->on('latest_table.latest_date', '=', 'which_day.created_at');
+        })->get();
+
+        return $data;
     }
+
 
     public static function votesSince($date, $activity_code){
         return self::select('wechat_name', DB::raw('MAX(which_day) as which_day'), DB::raw('MAX(time) as time'))
